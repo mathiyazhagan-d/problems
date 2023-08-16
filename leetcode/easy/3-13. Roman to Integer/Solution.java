6cacdad98305c31cb75daccb2200fd81fd0b63f3
mathiyazhagan-d <mathijo971@gmail.com>
2023-08-16 13:38:46 +0530

    dd

--- leetcode/easy/3-13. Roman to Integer/Solution.java
+++ leetcode/easy/3-13. Roman to Integer/Solution.java
@@ -36,6 +36,32 @@ import java.util.Map;
 
 public class Solution {
     public int romanToInt(String s) {
+//        int answer = 0, number = 0, prev = 0;
+//
+//        for (int j = s.length() - 1; j >= 0; j--) {
+//            switch (s.charAt(j)) {
+//                case 'M' -> number = 1000;
+//                case 'D' -> number = 500;
+//                case 'C' -> number = 100;
+//                case 'L' -> number = 50;
+//                case 'X' -> number = 10;
+//                case 'V' -> number = 5;
+//                case 'I' -> number = 1;
+//            }
+//            if (number < prev) {
+//                answer -= number;
+//            }
+//            else {
+//                answer += number;
+//            }
+//            prev = number;
+//        }
+//        return answer;
+
+
+
+
+
 Map<String,Integer> romanToNumber= new HashMap<>();
         romanToNumber.put("I",1);
         romanToNumber.put("V",5);
