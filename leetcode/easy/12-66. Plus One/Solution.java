ca1065b57fbf9e488f507c7f2ccad9005433b392
mathiyazhagan-d <mathijo971@gmail.com>
2023-08-27 14:22:21 +0530

    prop

---
+++ leetcode/easy/12-66. Plus One/Solution.java
@@ -0,0 +1,77 @@
+/*
+Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+Return k.
+Custom Judge:
+
+The judge will test your solution with the following code:
+
+int[] nums = [...]; // Input array
+int[] expectedNums = [...]; // The expected answer with correct length
+
+int k = removeDuplicates(nums); // Calls your implementation
+
+assert k == expectedNums.length;
+for (int i = 0; i < k; i++) {
+    assert nums[i] == expectedNums[i];
+}
+If all assertions pass, then your solution will be accepted.
+
+
+
+Example 1:
+
+Input: nums = [1,1,2]
+Output: 2, nums = [1,2,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 1 and 2 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+Example 2:
+
+Input: nums = [0,0,1,1,1,2,2,3,3,4]
+Output: 5, nums = [0,1,2,3,4,_,_,_,_,_]
+Explanation: Your function should return k = 5, with the first five elements of nums being 0, 1, 2, 3, and 4 respectively.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+
+*/
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        reverseArray(digits);
+
+        int carry = 1; // Initialize carry to 1 because we want to add 1
+
+        for (int i = 0; i < digits.length; i++) {
+            int sum = digits[i] + carry;
+            digits[i] = sum % 10; // Update current digit
+            carry = sum / 10;     // Update carry
+        }
+
+        if (carry > 0) {
+            int[] result = new int[digits.length + 1];
+            result[0] = carry;
+            for (int i = 1; i <= digits.length; i++) {
+                result[i] = digits[i - 1];
+            }
+            return result;
+        }
+
+        // Reverse the array back to the original order
+        reverseArray(digits);
+
+        return digits;
+    }
+
+    private void reverseArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+    }
+}
