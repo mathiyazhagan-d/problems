926818cdd9f51525152ea2c88b0b50b798ee4135
mathiyazhagan-d <mathijo971@gmail.com>
2023-09-11 10:16:27 +0530

    second change

--- leetcode/medium/GroupthePeopleGiventheGroupSizeTheyBelongTo.java
+++ leetcode/medium/GroupthePeopleGiventheGroupSizeTheyBelongTo.java
@@ -1,23 +1,22 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class GroupthePeopleGiventheGroupSizeTheyBelongTo {
     public List<List<Integer>> groupThePeople(int[] groupSizes) {
+        HashMap<Integer,List<Integer>> tempGroup = new HashMap<>();
         List<List<Integer>> result=new ArrayList<>();
 
         for(int i=0;i<groupSizes.length;i++){
-            if(groupSizes[i]>0){
-                int size=groupSizes[i];
-                List<Integer> temp=new ArrayList<>();
-                for(int j=0;j<groupSizes.length && temp.size()<size;j++){
-                    if(groupSizes[j]==size){
-                        temp.add(j);
-                        groupSizes[j]=0;
-                    }
-                }
-                if(temp.size()>0)
-                    result.add(temp);
+            int size=groupSizes[i];
+            tempGroup.putIfAbsent(size,new ArrayList<>());
+            tempGroup.get(size).add(i);
+
+            if(tempGroup.get(size).size()==size){
+                result.add(new ArrayList<>(tempGroup.get(size)));
+                tempGroup.get(size).clear();
             }
+
         }
         return result;
     }
