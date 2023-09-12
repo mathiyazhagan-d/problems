c24ad0387c3662b91b6f43bb7b57fc883f488bad
mathiyazhagan-d <mathijo971@gmail.com>
2023-09-12 13:47:38 +0530

    nnd

--- leetcode/easy/PascalsTriangleII.java
+++ leetcode/easy/PascalsTriangleII.java
@@ -1,23 +1,19 @@
 public class PascalsTriangleII {
     public List<Integer> getRow(int numRows) {
-
-
-        List<Integer> row=new ArrayList<>();
+        List<Integer> row = new ArrayList<>();
         row.add(1);
+        List<Integer> prev = row;
 
-
-        List<Integer> prev=row;
-
-        for(int i=1;i<=numRows;i++){
-            List<Integer> current=new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> current = new ArrayList<>();
             current.add(1);
-            for(int j=1;j<i;j++)
-                current.add(prev.get(j)+prev.get(j-1));
+            for (int j = 1; j < i; j++)
+                current.add(prev.get(j) + prev.get(j - 1));
             current.add(1);
 
-            if(i==numRows)
+            if (i == numRows)
                 return current;
-            prev=current;
+            prev = current;
         }
         return row;
 
