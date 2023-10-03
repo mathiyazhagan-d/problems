c6d6db4837b4c4e7789aae1c66c2f9564eb720f4
mathiyazhagan-d <116990455+mathiyazhagan-d@users.noreply.github.com>
2023-10-03 10:57:25 +0530

    jfj

--- leetcode/easy/ValidPalindrome.java
+++ leetcode/easy/ValidPalindrome.java
@@ -20,6 +20,7 @@ public class ValidPalindrome {
                      else {
                          start++;
                          last--;
+                         
                      }
             }
 
