f68230cc4a7b29f7abf9111c65e6dcff95c803cd
mathiyazhagan-d <mathijo971@gmail.com>
2023-10-09 20:50:03 +0530

    nbe

--- leetcode/easy/IsomorphicStrings.java
+++ leetcode/easy/IsomorphicStrings.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 public class IsomorphicStrings {
 
     public boolean isIsomorphic(String s, String t) {
@@ -16,5 +18,24 @@ public class IsomorphicStrings {
         }
         return true;
 
+    }
+    public boolean isIsomorphics(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        HashMap<Character,Character> hm=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            if(hm.containsKey(s.charAt(i))) {
+                if (!hm.get(s.charAt(i)).equals(t.charAt(i)))
+                    return false;
+            }else{
+                if(hm.containsValue(t.charAt(i)))
+                    return false;
+                else
+                hm.put(s.charAt(i),t.charAt(i));
+
+            }
+        }
+return true;
     }
 }
